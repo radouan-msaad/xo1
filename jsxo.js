5d876a3718de6ff74f58dfc1b7243001365917d2
radouan msaad <radwanmsaad3@gmail.com>
2021-03-06 10:31:24 +0100

    new msj

--- jsxo.js
+++ jsxo.js
@@ -1,118 +1,119 @@
 var i=0;
-		var m=0;
-		var dd;
-		var pos=0;
-		var pos1=0;
-		var p=[0,0];
-		var ply="x";
-		var tent=0;
-		var vll=[];
-		var y=[];
-		var rdm=[];
-		var vle=[];
-		var vl=[];
-		function audio1(type,nom,src,id) {
-			if(type=='class')
-				var doc=document.getElementsByClassName(nom)[0];
-			if(type=='tag')
-				var doc=document.getElementsByTagName(nom)[0];
-			if(type=='id')
-				var doc=document.getElementById(nom);
-			var src1=src+".mp3";
-			var audio=document.createElement("audio");
-			audio.setAttribute("src",src1);
-			audio.setAttribute("id",id);
-			audio.setAttribute("controls","");
-			audio.setAttribute("type","audio/mp3");
-			audio.setAttribute("autoplay","");
-			doc.append(audio);
-			audio.style.display="none";
-			audio.style.width="80%";
+var m=0;
+var dd;
+var pos=0;
+var pos1=0;
+var p=[0,0];
+var ply="x";
+var tent=0;
+var vll=[];
+var y=[];
+var rdm=[];
+var vle=[];
+var vl=[];
+function audio1(type,nom,src,id) {
+	if(type=='class')
+		var doc=document.getElementsByClassName(nom)[0];
+	if(type=='tag')
+		var doc=document.getElementsByTagName(nom)[0];
+	if(type=='id')
+		var doc=document.getElementById(nom);
+	var src1=src+".mp3";
+	var audio=document.createElement("audio");
+	audio.setAttribute("src",src1);
+	audio.setAttribute("id",id);
+	audio.setAttribute("controls","");
+	audio.setAttribute("type","audio/mp3");
+	audio.setAttribute("autoplay","");
+	doc.append(audio);
+	audio.style.display="none";
+	audio.style.width="80%";
+}
+function play(){
+	var r=14;
+	for(var l=1;l<10;l++)
+		vl[l-1]= document.getElementById('d'+l);
+	for(var k=0;k<9;k++)
+		vll[k]=k+1;
+	audio1('tag','body','win3','l2');
+	var capt=document.getElementById('cc');
+	var div=document.getElementsByClassName('d');
+	for (var k =0;k<9;k++) {
+		tent=0;
+		if(k==r-1);
+		else{
+			ply="x";
+			div[k].style.pointerEvents="auto";
+			div[k].style.backgroundColor="aqua";
+			div[k].setAttribute("value",(k+1));
+			div[k].innerHTML="";
+			capt.innerHTML="jeu xo";
+			capt.style.color="red";
+			capt.style.backgroundColor="white";
 		}
-		function play(){
-			var r=14;
-			for(var l=1;l<10;l++)
-			vl[l-1]= document.getElementById('d'+l);
-		    for(var k=0;k<9;k++)
-			vll[k]=k+1;
-			audio1('tag','body','win3','l2');
-			var capt=document.getElementById('cc');
-			var div=document.getElementsByClassName('d');
-			for (var k =0;k<9;k++) {
-				tent=0;
-				if(k==r-1);
-				else{
-				ply="x";
-				div[k].style.pointerEvents="auto";
-				div[k].style.backgroundColor="aqua";
-				div[k].setAttribute("value",(k+1));
-				div[k].innerHTML="";
-				capt.innerHTML="jeu xo";
-				capt.style.color="red";
-				capt.style.backgroundColor="white";
-			}
-			}
-		}
-		function change(obj){
-			i=0;
-			m=0;
-			for(var q=0;q<10;q++)
-				y[q]="";
-			var btn=document.getElementsByClassName('d');
-			var point=document.getElementsByClassName('point');
-			var capt=document.getElementById('cc');
-			var ply=document.getElementById('ply');
-			for(var l=1;l<10;l++)
-			vl[l-1]= document.getElementById('d'+l);
-		   for(var k=0;k<9;k++){
-		   	if(k==r-1);
-		   else
+	}
+}
+function change(obj){
+	i=0;
+	m=0;
+	for(var q=0;q<10;q++)
+		y[q]="";
+	var btn=document.getElementsByClassName('d');
+	var point=document.getElementsByClassName('point');
+	var capt=document.getElementById('cc');
+	var ply=document.getElementById('ply');
+	for(var l=1;l<10;l++)
+		vl[l-1]= document.getElementById('d'+l);
+	for(var k=0;k<9;k++){
+		if(k==r-1);
+		else
 			vll[k]= vl[k].getAttribute("value");}
-			if(i%2==0){
-				switch(obj){
-					case '1':vle[0]=1; break;
-					case '2':vle[1]=2; break;
-					case '3':vle[2]=3; break;
-					case '4':vle[3]=4; break;
-					case '5':vle[4]=5; break;
-					case '6':vle[5]=6; break;
-					case '7':vle[6]=7; break;
-					case '8':vle[7]=8; break;
-					case '9':vle[8]=9; break;
-				}
-				vl[obj-1].setAttribute("value","x");
-				vl[obj-1].innerHTML="x";
-				vl[obj-1].style.backgroundColor="lightgreen";
-				vl[obj-1].style.pointerEvents="none";
-				vll[obj-1]="x";
-				for(var z=0;z<9;z++){
-					if((vll[z]=='x')||(vll[z]=='o'))
+		if(i%2==0){
+			switch(obj){
+				case '1':vle[0]=1; break;
+				case '2':vle[1]=2; break;
+				case '3':vle[2]=3; break;
+				case '4':vle[3]=4; break;
+				case '5':vle[4]=5; break;
+				case '6':vle[5]=6; break;
+				case '7':vle[6]=7; break;
+				case '8':vle[7]=8; break;
+				case '9':vle[8]=9; break;
+			}
+			vl[obj-1].setAttribute("value","x");
+			vl[obj-1].innerHTML="x";
+			vl[obj-1].style.backgroundColor="lightgreen";
+			vl[obj-1].style.pointerEvents="none";
+			vll[obj-1]="x";
+			vr();
+			for(var z=0;z<9;z++){
+				if((vll[z]=='x')||(vll[z]=='o'))
 					;
-					else{ y[m]=z+1;
-						m++;
-					}
+				else{ y[m]=z+1;
+					m++;
 				}
-				var r=Math.floor(Math.random()*(m));
-				switch(y[r]){
-					case 1:pos=1; break;
-					case 2:pos=2; break;
-					case 3:pos=3; break;
-					case 4:pos=4; break;
-					case 5:pos=5; break;
-					case 6:pos=6; break;
-					case 7:pos=7; break;
-					case 8:pos=8; break;
-					case 9:pos=9; break;
-				}
-				if((vll[0]=="o")&&(vll[3]=="o")&&(vll[6]==7)){
-					pos=7;
-				}
-				else if((vll[3]=="o")&&(vll[6]=="o")&&(vll[0]==1)){
-					pos=1;
-				}
-				else if((vll[0]=="o")&&(vll[6]=="o")&&(vll[3]==4)){
-					pos=4;
-				}
+			}
+			var r=Math.floor(Math.random()*(m));
+			switch(y[r]){
+				case 1:pos=1; break;
+				case 2:pos=2; break;
+				case 3:pos=3; break;
+				case 4:pos=4; break;
+				case 5:pos=5; break;
+				case 6:pos=6; break;
+				case 7:pos=7; break;
+				case 8:pos=8; break;
+				case 9:pos=9; break;
+			}
+			if((vll[0]=="o")&&(vll[3]=="o")&&(vll[6]==7)){
+				pos=7;
+			}
+			else if((vll[3]=="o")&&(vll[6]=="o")&&(vll[0]==1)){
+				pos=1;
+			}
+			else if((vll[0]=="o")&&(vll[6]=="o")&&(vll[3]==4)){
+				pos=4;
+			}
 				//2
 				else if((vll[0]=="o")&&(vll[1]=="o")&&(vll[2]==3)){
 					pos=3;
@@ -159,7 +160,7 @@ var i=0;
 				}
 				else if((vll[3]=="o")&&(vll[5]=="o")&&(vll[4]==5)){					
 					pos=5;
-			    }
+				}
 				else if((vll[4]=="o")&&(vll[5]=="o")&&(vll[3]==4)){
 					pos=4;
 				}
@@ -273,10 +274,12 @@ var i=0;
 
 			
 			m=0;
+			vr();
+			function vr(){
 			if(((vll[0]==vll[1])&&(vll[0]==vll[2]))||((vll[0]==vll[3])&&(vll[0]==vll[6]))||((vll[0]==vll[4])&&(vll[0]==vll[8]))||((vll[8]==vll[5])&&(vll[8]==vll[2]))||((vll[8]==vll[7])&&(vll[8]==vll[6]))||((vll[5]==vll[4])&&(vll[5]==vll[3]))||((vll[1]==vll[4])&&(vll[1]==vll[7]))||((vll[2]==vll[4])&&(vll[2]==vll[6]))){
 				var t=setTimeout(function(){
-						play();
-					},3000);
+					play();
+				},3000);
 				if((vll[0]==vll[1])&&(vll[0]==vll[2])){
 					
 					for(var j=0;j<3;j++){
@@ -298,7 +301,7 @@ var i=0;
 							ply="o";i=1;
 							vl[j].style.backgroundColor="red";
 						}
-					else if(vll[j]=="x")  {
+						else if(vll[j]=="x")  {
 							ply="x";i=0;
 							vl[j].style.backgroundColor="green";
 						}
@@ -310,7 +313,7 @@ var i=0;
 							ply="o";i=1;
 							vl[j].style.backgroundColor="red";
 						}
-					else if(vll[j]=="x") {
+						else if(vll[j]=="x") {
 							ply="x";i=0;
 							vl[j].style.backgroundColor="green";
 						}
@@ -322,7 +325,7 @@ var i=0;
 							ply="o";i=1;
 							vl[j].style.backgroundColor="red";
 						}
-					else if(vll[j]=="x") {
+						else if(vll[j]=="x") {
 							ply="x";i=0;
 							vl[j].style.backgroundColor="green";
 						}
@@ -346,7 +349,7 @@ var i=0;
 							ply="o";i=1;
 							vl[j].style.backgroundColor="red";
 						}
-					else if(vll[j]=="x") {
+						else if(vll[j]=="x") {
 							ply="x";i=0;
 							vl[j].style.backgroundColor="green";
 						}
@@ -358,7 +361,7 @@ var i=0;
 							ply="o";i=1;
 							vl[j].style.backgroundColor="red";
 						}
-					else if(vll[j]=="x") {
+						else if(vll[j]=="x") {
 							ply="x";i=0;
 							vl[j].style.backgroundColor="green";
 						}
@@ -388,14 +391,14 @@ var i=0;
 					if(ply=="x"){
 						
 						if(j==0)
-						p[1]++;
+							p[1]++;
 						point[1].innerHTML="x: "+p[1];
 						capt.style.backgroundColor="green";
 					}
 					else{
 						
 						if(j==0)
-						p[0]++;
+							p[0]++;
 						point[0].innerHTML="o: "+p[0];
 						capt.style.backgroundColor="red";
 					}
@@ -409,22 +412,22 @@ var i=0;
 			else tent++;
 			audio1('tag','body','click1','l2');
 			for(var z=0;z<9;z++){
-					if((vl[z].getAttribute("value")=='x')||(vl[z].getAttribute("value")=='o'))
+				if((vl[z].getAttribute("value")=='x')||(vl[z].getAttribute("value")=='o'))
 					;
-					else{ y[m]=z+1;
-						m++;
-					}
+				else{ y[m]=z+1;
+					m++;
 				}
+			}
 			if(m==0){
 				m=0;
 				var t=setTimeout(function(){
-						play();
-					},3000);
+					play();
+				},3000);
 				audio1('tag','body','false2','l2');
 				document.getElementById("cc").innerHTML="NULL";
 				for(var j=0;j<10;j++){
 					vl[j].style.backgroundColor="yellow";
-						vll[j]=j+1;
+					vll[j]=j+1;
 				}
 
 			}
@@ -433,3 +436,4 @@ var i=0;
 				ply.style.backgroundColor="white";
 			}
 		}
+	}
